78d047fe926adb5a24a98302542e0a0ded4002aa
kjjd84 <kjjd84@icloud.com>
2026-02-21 00:56:02 -0500

    fix filament cmd

--- src/Console/FilamentCommand.php
+++ src/Console/FilamentCommand.php
@@ -3,6 +3,7 @@
 namespace Kjjd84\Lucid\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
 
 class FilamentCommand extends Command
 {
@@ -12,36 +13,28 @@ class FilamentCommand extends Command
 
     public function handle(): void
     {
-        exec('composer require filament/filament:"^5.0"');
+        $this->components->task('Requiring Filament via composer', function () {
+            Process::run('composer require filament/filament:"^5.0"');
+        });
 
-        require_once base_path('vendor/autoload.php');
-
-        $this->copyStubs([
+        $paths = [
             'app/Providers/AdminPanelProvider.php',
             'bootstrap/providers.php',
             'config/filament.php',
             'routes/web.php',
-        ]);
+        ];
 
-        $this->call(ModelCommand::class, [
-            'name' => 'User',
-            '--resource' => true,
-            '--force' => true,
-        ]);
-    }
-
-    public function copyStubs($filenames)
-    {
-        foreach ($filenames as $filename) {
-            $this->components->task(
-                "Copying $filename stub",
-                function () use ($filename) {
-                    file_put_contents(
-                        base_path($filename),
-                        file_get_contents(__DIR__ . '/../../stubs/filament/' . basename($filename)),
-                    );
-                }
-            );
+        foreach ($paths as $path) {
+            $this->components->task("Copying $path stub", function () use ($path) {
+                file_put_contents(
+                    base_path($path),
+                    file_get_contents(__DIR__ . '/../../stubs/filament/' . basename($path)),
+                );
+            });
         }
+
+        $this->components->task('Creating user model & resource', function () {
+            Process::run('php artisan lucid:model User -r --force');
+        });
     }
 }
